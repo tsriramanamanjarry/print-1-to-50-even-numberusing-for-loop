0ff63b2203c6bf72e3b82b205f5a3443f2b2e4d1
tsriramanamanjarry <143001940+tsriramanamanjarry@users.noreply.github.com>
2024-10-04 13:57:35 +0530

    Create even.java

---
+++ even.java
@@ -0,0 +1,13 @@
+class HelloWorld {
+    public static void main(String[] args) {
+      for(int i=1;i<=50;i++){
+      if ( i%2==0){
+      
+      System.out.println(i);
+      }
+      else{
+      continue;
+      }
+      }
+    }
+}
